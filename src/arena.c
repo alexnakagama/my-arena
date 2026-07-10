ebdaf78ccc95382e8133684e8e0de70166b66b66
alexnakagama <alexnakagama4@gmail.com>
2026-07-09 23:00:43 -0300

    structure of arena

--- src/arena.c
+++ src/arena.c
@@ -1,8 +1,15 @@
 #include "arena/arena.h"
 #include <stddef.h>
+#include <stdlib.h>
 
 struct Arena {
     void *buffer;
     size_t capacity;
     size_t offset;
 };
+
+Arena *arena_create(size_t size) {
+    Arena *arena = malloc(sizeof(Arena));
+
+    return arena;
+}
