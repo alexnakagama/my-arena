8ea9c9346bcb3bdca735007714c646f500886298
alexnakagama <alexnakagama4@gmail.com>
2026-07-13 00:44:24 -0300

    arena print

--- src/arena.c
+++ src/arena.c
@@ -93,4 +93,8 @@ void arena_print(const arena_t *arena) {
     if (!arena) {
         return;
     }
+    printf("---Arena---\n");
+    printf("Buffer: %p\n", arena->buffer);
+    printf("Offset: %zu\n", arena->offset);
+    printf("Capacity%zu\n", arena->capacity);
 }
