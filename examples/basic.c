49c1326dd39dc20068770e27346be077efc5bec9
alexnakagama <alexnakagama4@gmail.com>
2026-07-13 15:11:07 -0300

    main

--- examples/basic.c
+++ examples/basic.c
@@ -1,16 +1,17 @@
 #include "arena/arena.h"
 
 int main(void) {
-    arena_t *arena;
-
-    arena = arena_create(1024);
+    arena_t *arena = arena_create(1024);
 
     int *number = arena_alloc(arena, sizeof(int));
 
     *number = 42;
 
     arena_info(arena);
+
     arena_dump(arena);
+
+    arena_destroy(arena);
 
     return 0;
 }
