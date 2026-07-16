61440ad90e0a9e5a0d7709abd5da479f2e11fd03
alexnakagama <alexnakagama4@gmail.com>
2026-07-16 13:29:41 -0300

    finished test for destroy

--- tests/test_destroy.c
+++ tests/test_destroy.c
@@ -3,5 +3,12 @@
 #include "arena/arena.h"
 
 int main(void) {
+    arena_t *arena = arena_create(1024);
+    assert(arena != NULL);
+
+    arena_destroy(arena);
+
+    printf("test_destroy.c PASSED\n");
+
     return 0;
 }
