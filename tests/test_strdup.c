2da450658b174198d0800d09908a8c2daa57518c
alexnakagama <alexnakagama4@gmail.com>
2026-07-16 14:02:48 -0300

    improved the test

--- tests/test_strdup.c
+++ tests/test_strdup.c
@@ -1,7 +1,24 @@
 #include <assert.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <string.h>
 #include "arena/arena.h"
 
 int main(void){
+    arena_t *arena = arena_create(1024);
+    assert(arena != NULL);
+    assert(arena_capacity(arena) == 1024);
+
+    char *my_str = arena_strdup(arena, "hello");
+    assert(my_str != NULL);
+
+    size_t str_length = strlen("hello") + 1;
+    assert(arena_used(arena) == str_length);
+    assert(arena_available(arena) == 1024 - str_length);
+    
+    arena_destroy(arena);
+
+    printf("test_strdup.c PASSED\n");
+
     return 0;
 }
