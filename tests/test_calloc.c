2a0c60d94129c30db3d577be6f1b4fcd2a2aaa6c
alexnakagama <alexnakagama4@gmail.com>
2026-07-16 13:43:34 -0300

    test calloc finisgh

--- tests/test_calloc.c
+++ tests/test_calloc.c
@@ -1,7 +1,27 @@
 #include <assert.h>
+#include <stddef.h>
 #include <stdio.h>
 #include "arena/arena.h"
 
 int main(void) {
+    arena_t *arena = arena_create(1024);
+    assert(arena != NULL);
+
+    int *numbers = arena_calloc(arena, 5, sizeof(int));
+    assert(numbers != NULL);
+
+    size_t count = 5;
+
+    for (size_t i = 0; i < count; i++) {
+        assert(numbers[i] == 0);
+    }
+
+    numbers[0] = 10; 
+    assert(numbers[0] == 10);
+
+    arena_destroy(arena);
+
+    printf("test_calloc.c PASSED\n");
+
     return 0;
 }
