6432f6eb1ec9108fd798e732416b6d9c9be8ec38
alexnakagama <alexnakagama4@gmail.com>
2026-07-16 13:51:54 -0300

    improved the unit test for test calloc

--- tests/test_calloc.c
+++ tests/test_calloc.c
@@ -6,15 +6,18 @@
 int main(void) {
     arena_t *arena = arena_create(1024);
     assert(arena != NULL);
-
-    int *numbers = arena_calloc(arena, 5, sizeof(int));
-    assert(numbers != NULL);
+    assert(arena_capacity(arena) == 1024);
 
     size_t count = 5;
+
+    int *numbers = arena_calloc(arena, count, sizeof(int));
+    assert(numbers != NULL);
 
     for (size_t i = 0; i < count; i++) {
         assert(numbers[i] == 0);
     }
+    assert(arena_used(arena) == count * sizeof(int));
+    assert(arena_available(arena) == 1024 - (count * sizeof(int)));
 
     numbers[0] = 10; 
     assert(numbers[0] == 10);
